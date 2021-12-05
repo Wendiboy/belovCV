a20d3569050f1062f164f430824996277cbedba2
Wendiboy <belov.misha00@gmail.com>
2021-12-05 15:02:20 +0700

    add Main_component structure

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,8 +4,25 @@ import './Main.css';
 const Main = () => {
     return (
         <div className='Main'>
+            <div className='Main__wrapper'>
+                <div className='Main__photo'>
 
+                </div>
+                <div className='Main__info'>
+                    <div className='fullname'>Mikhail Belov</div>
+                    <div className='job_title'>Frontend developer</div>
+                    <div className='mail'>mimikrichik@gmail.com</div>
+                    <div className='phone'>+7 996 304 19 12</div>
+                    <div className='location'>Novosibirsk</div>
+                    <div className='social_networks'>
+                        <span className='github'></span>
+                        <span className='instagram'></span>
+                        <span className='vk'></span>
+                        <span className='telegram'></span>
+                    </div>
+                </div>
 
+            </div>
         </div>
     )
 }
