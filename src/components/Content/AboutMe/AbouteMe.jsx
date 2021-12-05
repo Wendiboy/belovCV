e9ef861bee018ad451b5bfaf16c0a920a9c0bd35
Wendiboy <belov.misha00@gmail.com>
2021-12-05 18:05:14 +0700

    add AboutMe component

---
+++ src/components/Content/AboutMe/AbouteMe.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './AboutMe.css';
+
+const AboutMe = () => {
+    return (
+        <div className='AboutMe'>
+            <h2 className='AboutMe__header'>Aboute me</h2>
+            <p className='AboutMe__text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+                aspernatur, aut consectetur dicta iure molestiae nam obcaecati odio omnis optio quo,
+                repellendus rerum similique sint tenetur totam vel voluptas. Praesentium!
+            </p>
+            <h2 className='AboutMe__header'>What I do</h2>
+            <div className='technologies_list'>
+                <div className='technologies__elem'>React</div>
+                <div className='technologies__elem'>HTML</div>
+                <div className='technologies__elem'>CSS</div>
+            </div>
+            <h2 className='AboutMe__header'>Key skills</h2>
+            <div className='skills_list'>
+                <div className='skills__elem'>Ответственность</div>
+                <div className='skills__elem'>Усидчивость</div>
+                <div className='skills__elem'>Стрессоустойчивость</div>
+                <div className='skills__elem'>Пользователь ПК</div>
+            </div>
+            <h2 className='AboutMe__header'>Languages</h2>
+                <div className='lang__item'>Русский — Родной</div>
+                <div className='lang__item'>Английский — B2 — Средне-продвинутый</div>
+        </div>
+    )
+}
+
+export default AboutMe
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import './Content.css';
+import AbouteMe from "./AboutMe/AbouteMe.jsx";
 
 const Content = () => {
     return (
         <div className='Content'>
+            <AbouteMe />
 
         </div>
     )
