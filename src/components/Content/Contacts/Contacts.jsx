5a8029f0ead063c2a7d89311e7a5da2174e6c56c
Wendiboy <belov.misha00@gmail.com>
2021-12-06 10:05:43 +0700

    some refactoring

--- src/components/Content/Contacts/Contacts.jsx
+++ src/components/Content/Contacts/Contacts.jsx
@@ -9,17 +9,17 @@ const Contacts = () => {
                 <span className='Contact_element'>
                     <div className='contact_element__name'>Email</div>
                     <div className='contact_element__text'>mimikrichik@gmail.com</div>
-                    <div className='contact_element__icon'></div>
+                    <img className='contact_element__icon' src={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_184197.png&f=1&nofb=1`}></img>
                 </span>
                 <span className='Contact_element'>
                     <div className='contact_element__name'>Phone</div>
                     <div className='contact_element__text'>+7 996 304 19 12</div>
-                    <div className='contact_element__icon'></div>
+                    <img className='contact_element__icon' src={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwebstockreview.net%2Fimages%2Fphone-icon-png-3.png&f=1&nofb=1`}></img>
                 </span>
                 <span className='Contact_element'>
                     <div className='contact_element__name'>Telegram</div>
                     <div className='contact_element__text'>@wendiboy</div>
-                    <div className='contact_element__icon'></div>
+                    <img className='contact_element__icon' src={`https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_1703.png&f=1&nofb=1`}></img>
                 </span>
             </div>
         </div>
